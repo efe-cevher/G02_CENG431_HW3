084177bdd7ed6c802ad5bf8afb26ccf6b123c758
Zekihan <zekihanazman@std.iyte.edu.tr>
2020-05-20 17:40:11 +0300

    json&xml start

--- src/tube/JSONFormatter.java
+++ src/tube/JSONFormatter.java
@@ -1,4 +1,19 @@
 package tube;
 
+import com.google.gson.Gson;
+
 public class JSONFormatter {
+    Gson gson;
+
+    public JSONFormatter() {
+        this.gson = new Gson();
+    }
+
+    public String toJson(Video video){
+        return gson.toJson(video);
+    }
+
+    public Video fromJson(String video){
+        return gson.fromJson(video,Video.class);
+    }
 }
