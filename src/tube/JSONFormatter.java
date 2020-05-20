23662794c8da6a91f457cfe50509e171f4d978df
Zekihan <zekihanazman@std.iyte.edu.tr>
2020-05-20 17:58:55 +0300

    json

--- src/tube/JSONFormatter.java
+++ src/tube/JSONFormatter.java
@@ -2,6 +2,10 @@ package tube;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class JSONFormatter {
     Gson gson;
 
@@ -15,5 +19,20 @@ public class JSONFormatter {
 
     public Video fromJson(String video){
         return gson.fromJson(video,Video.class);
+    }
+
+    public static void main(String[] args) {
+        List<Comment> comments = new ArrayList<>();
+        comments.add(new Comment("asd1","asd1"));
+        comments.add(new Comment("asd2","asd2"));
+        comments.add(new Comment("asd3","asd3"));
+        comments.add(new Comment("asd4","asd4"));
+        Video video = new Video(1,"video1","video1content",new Date(),5,1,comments);
+        JSONFormatter jsonFormatter = new JSONFormatter();
+        String s = jsonFormatter.toJson(video);
+        System.out.println(s);
+        Video video1 = jsonFormatter.fromJson(s);
+        String s1 = jsonFormatter.toJson(video1);
+        System.out.println(s1);
     }
 }
