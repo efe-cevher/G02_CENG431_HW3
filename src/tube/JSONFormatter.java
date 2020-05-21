3e39c5d56140f679d1bd1ac0039248f5913e394d
Zekihan <zekihanazman@std.iyte.edu.tr>
2020-05-21 16:34:45 +0300

    videos.json file created&json formmatter now takes map

--- src/tube/JSONFormatter.java
+++ src/tube/JSONFormatter.java
@@ -1,38 +1,48 @@
 package tube;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.lang.reflect.Type;
+import java.util.*;
 
-public class JSONFormatter implements IFormatter<Video>{
-    Gson gson;
+public class JSONFormatter implements IFormatter<Map<Integer,Video>>{
+    private Gson gson;
 
     public JSONFormatter() {
-        this.gson = new Gson();
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
     }
 
     public String toFormat(Video video){
         return gson.toJson(video);
     }
 
-    public Video toObject(String video){
+    public Video toVideoObject(String video){
         return gson.fromJson(video,Video.class);
     }
 
+    public Map<Integer,Video> toObject(String videoMap){
+        Type videoMapType = new TypeToken<Map<Integer,Video>>() {}.getType();
+        return gson.fromJson(videoMap,videoMapType);
+    }
+
+    public String toFormat(Map<Integer,Video> videoMap){
+        return gson.toJson(videoMap);
+    }
+
     public static void main(String[] args) {
-        List<Comment> comments = new ArrayList<>();
-        comments.add(new Comment("asd1","asd1"));
-        comments.add(new Comment("asd2","asd2"));
-        comments.add(new Comment("asd3","asd3"));
-        comments.add(new Comment("asd4","asd4"));
-        Video video = new Video(1,"video1","video1content",new Date(),5,1,comments);
+        Map<Integer,Video> videoMap = new HashMap<>();
+        for (int i = 1; i < 51; i++) {
+            Video video = new Video(i,"video "+ i,"video " + i + " content",new Date(),0,0,new ArrayList<>());
+            videoMap.put(i,video);
+        }
+
         JSONFormatter jsonFormatter = new JSONFormatter();
-        String s = jsonFormatter.toFormat(video);
-        System.out.println(s);
-        Video video1 = jsonFormatter.toObject(s);
-        String s1 = jsonFormatter.toFormat(video1);
-        System.out.println(s1);
+        String s = jsonFormatter.toFormat(videoMap);
+        IStorage storage = new FileStorage("videos.json");
+        storage.save(s);
+
+        //Map<Integer,Video> videoMap2 = jsonFormatter.toObject(s);
     }
 }
