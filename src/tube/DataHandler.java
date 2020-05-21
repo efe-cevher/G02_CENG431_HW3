b09792da3e172a0e225d8db9d24a75ca7d72d855
Efe Cevher <37309169+efe-cevher@users.noreply.github.com>
2020-05-21 12:38:08 +0300

    datahandler init

--- src/tube/DataHandler.java
+++ src/tube/DataHandler.java
@@ -1,4 +1,33 @@
 package tube;
 
-public class DataHandler {
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataHandler implements IDataHandler{
+    Map<String,User> userMap;
+    Map<Integer,Video> videoMap;
+
+    public DataHandler() {
+        //load from xml and json
+        this.userMap = new HashMap<>();
+        this.videoMap = new HashMap<>();
+    }
+
+    public User getUser(String username){
+        return userMap.get(username);
+    }
+
+    public Video getVideo(int id){
+        return videoMap.get(id);
+    }
+
+    public void putUser(User user){
+        userMap.put(user.getUsername(), user);
+        //Also update xml
+    }
+
+    public void putVideo(Video video){
+        videoMap.put(video.getId(), video);
+        //Also update json
+    }
 }
--- src/tube/IDataHandler.java
+++ src/tube/IDataHandler.java
@@ -1,4 +1,11 @@
 package tube;
 
 public interface IDataHandler {
+    public User getUser(String username);
+
+    public Video getVideo(int id);
+
+    public void putUser(User user);
+
+    public void putVideo(Video video);
 }
