7e18d3970f43d7ffcec9409386d79660c757f80a
Zekihan <zekihanazman@std.iyte.edu.tr>
2020-05-22 05:17:33 +0300

    warning removed

--- src/tube/XMLFormatter.java
+++ src/tube/XMLFormatter.java
@@ -1,13 +1,20 @@
 package tube;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.converters.basic.*;
+import com.thoughtworks.xstream.converters.collections.CollectionConverter;
+import com.thoughtworks.xstream.converters.reflection.ReflectionConverter;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+import com.thoughtworks.xstream.security.NoTypePermission;
+import com.thoughtworks.xstream.security.WildcardTypePermission;
 
 import javax.xml.*;
 import javax.xml.bind.*;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 import java.util.*;
 
 public class XMLFormatter{
@@ -104,11 +111,67 @@ public class XMLFormatter{
         return xstream.toXML(users);
     }
 
-    public Map<String,User> fromFormat(String data){
-        XStream xstream = new XStream();
+    public XStream createXStream() {
+        XStream xstream = new XStream(new StaxDriver() {
+            @Override
+            public HierarchicalStreamWriter createWriter(Writer out) {
+                return new PrettyPrintWriter(out, "    ");
+            }
+        }) {
+            // only register the converters we need; other converters generate a private access warning in the console on Java9+...
+            @Override
+            protected void setupConverters() {
+                registerConverter(new NullConverter(), PRIORITY_VERY_HIGH);
+                registerConverter(new IntConverter(), PRIORITY_NORMAL);
+                registerConverter(new FloatConverter(), PRIORITY_NORMAL);
+                registerConverter(new DoubleConverter(), PRIORITY_NORMAL);
+                registerConverter(new LongConverter(), PRIORITY_NORMAL);
+                registerConverter(new ShortConverter(), PRIORITY_NORMAL);
+                registerConverter(new BooleanConverter(), PRIORITY_NORMAL);
+                registerConverter(new ByteConverter(), PRIORITY_NORMAL);
+                registerConverter(new StringConverter(), PRIORITY_NORMAL);
+                registerConverter(new DateConverter(), PRIORITY_NORMAL);
+                registerConverter(new CollectionConverter(getMapper()), PRIORITY_NORMAL);
+                registerConverter((new ReflectionConverter(getMapper(), getReflectionProvider()){
+                    @Override
+                    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+                        Object result = new Users(new ArrayList<>());
+                        result = this.doUnmarshal(result, reader, context);
+                        return this.serializationMembers.callReadResolve(result);
+                    }
+
+                    @Override
+                    public boolean canConvert(Class type) {
+                        return type.equals(Users.class);
+                    }
+                }),PRIORITY_VERY_LOW);
+                registerConverter((new ReflectionConverter(getMapper(), getReflectionProvider()){
+                    @Override
+                    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+                        Object result = new User();
+                        result = this.doUnmarshal(result, reader, context);
+                        return this.serializationMembers.callReadResolve(result);
+                    }
+                    @Override
+                    public boolean canConvert(Class type) {
+                        return type.equals(User.class);
+                    }
+                }),PRIORITY_VERY_LOW);
+            }
+        };
+
+        xstream.autodetectAnnotations(true);
+        xstream.addPermission(NoTypePermission.NONE);
+        xstream.addPermission(new WildcardTypePermission(new String[] {Users.class.getName(), User.class.getName()}));
         xstream.alias("user", User.class);
         xstream.alias("users", Users.class);
         xstream.addImplicitCollection(Users.class, "users");
+
+        return xstream;
+    }
+
+    public Map<String,User> fromFormat(String data){
+        XStream xstream = createXStream();
         String xmlContent = "";
         Users users = (Users)xstream.fromXML(data);
         Map<String,User> userMap = new HashMap<>();
